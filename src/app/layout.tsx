caf29875a857600175a55df76902f7d1ca5227f3
HPHuerta <hhernanp@gmail.com>
2023-06-08 23:57:17 -0400

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='font-plusJakarta'>{children}</body>
     </html>
   )
 }

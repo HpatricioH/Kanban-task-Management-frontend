3e39e1d0ddf8b7a9e4dc76562aa5cabab6eb4e11
HPHuerta <hhernanp@gmail.com>
2023-06-10 01:17:44 -0400

    feat: added navbar and button component to be reuseable

---
+++ src/app/core/utils/Button.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+interface ButtonType {
+  children?: React.ReactNode
+  icon: string
+  buttonStyle: string
+}
+
+export const Button = ({ children, icon, buttonStyle }: ButtonType) => {
+  return (
+    <button className={buttonStyle}>{children}
+      <Image
+        src={icon}
+        alt="button icon"
+        width={12}
+        height={12}
+      />
+    </button>
+  )
+}

cbddf2ed2bec492cf628e3f92ec1b03dddb2dc3f
HPHuerta <hhernanp@gmail.com>
2023-06-15 00:51:05 -0400

    feat: menu style completed

--- src/app/components/BoardsMenu/BoardsMenu.tsx
+++ src/app/components/BoardsMenu/BoardsMenu.tsx
@@ -31,28 +31,30 @@ export default function BoardsMenu ({ setShowModal }: ShowModal) {
       id='modal'
       onClick={(e) => { handleClose(e) }}>
 
-      <div className='bg-[#2B2C37] rounded-md flex flex-col gap-4 w-full'>
+      <div className='bg-[#2B2C37] rounded-md flex flex-col gap-4 shadow-lg shadow-[#364e7e40]/25 absolute w-[18rem] top-[4.7rem]'>
 
-        <h1 className='uppercase px-4 pt-4'>All Boards (3)</h1>
+        <h1 className='uppercase px-4 pt-4 text-[#828FA3] font-bold text-xs tracking-[0.15rem] leading-[0.938rem]'>All Boards (3)</h1>
+
         {boards.map((board) => (
-          <div key={board.id} className='bg-[#635FC7] w-[16rem] rounded-r-3xl text-[#FFFFFF] font-semibold'>
+          <div key={board.id} className='bg-[#635FC7] w-[15rem] rounded-r-3xl text-[#FFFFFF] font-semibold  text-[0.938rem] leading-[1.188rem]'>
             <div className='flex pl-4 gap-2 py-4'>
               <Image
                 src='./icons/icon-board.svg'
                 alt='board icon'
-                width={18}
-                height={18}
+                width={22}
+                height={8}
               />
               <p>{board.name}</p>
             </div>
           </div>
         ))}
-        <div className='flex gap-3 pl-4 text-[#635FC7] font-semibold'>
+
+        <div className='flex gap-3 pl-4 text-[#635FC7] font-semibold text-[0.938rem] leading-[1.188rem]'>
           <Image
             src='./icons/icon-board-create.svg'
             alt='board icon'
-            width={18}
-            height={18}
+            width={22}
+            height={8}
           />
           <p>+ Create New Board</p>
         </div>
@@ -63,11 +65,15 @@ export default function BoardsMenu ({ setShowModal }: ShowModal) {
             width={18}
             height={18}
           />
- <button
-            id="theme-toggle"
-            type="button"
-            className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200  rounded-lg text-sm p-2.5"
-          ></button>
+          <label className="flex cursor-pointer select-none items-center">
+            <div className="relative">
+              <input type="checkbox" id="toggleFour" className="sr-only" />
+              <div className="box bg-[#635FC7] block h-6 w-14 rounded-full"></div>
+              <div
+                className="dot absolute right-1 top-[0.25rem] flex h-[1.05rem] w-[1.05rem] items-center justify-center rounded-full bg-white transition"
+              ></div>
+            </div>
+          </label>
           <Image
             src='./icons/icon-dark-theme.svg'
             alt='board icon'

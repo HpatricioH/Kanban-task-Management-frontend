afd0e022f9c1ae5743fa3c296ea134bec1185343
HPHuerta <hhernanp@gmail.com>
2023-06-28 00:26:26 -0400

    feat: taskcards initiated, button for tasks style configured

--- src/app/components/Main/MainColumn.tsx
+++ src/app/components/Main/MainColumn.tsx
@@ -11,10 +11,10 @@ export default function MainColumn () {
   // TODO: add a button to create a new board if the board is not selected or the board is empty
 
   return (
-    <section className='h-auto flex flex-col justify-evenly items-center gap-6'>
+    <section className='h-auto flex justify-around items-center gap-6 w-full p-4'>
       {column?.length === 0 || board.board.length === 0
         ? (
-          <>
+          <div className='flex flex-col justify-evenly items-center gap-6'>
             <p className='text-center font-bold text-[#828FA3] text-[1.125rem] leading-[1.438rem]'>
               This board is empty. Create a new column to get started.
             </p>
@@ -24,11 +24,22 @@ export default function MainColumn () {
             >
               Add New Column
             </Button>
-          </>
+          </div>
           )
         : column?.map((col: Column) => (
           <div key={col.id}>
-            <p>{col.name}</p>
+            <div className='flex justify-center items-baseline'>
+              <div className={`rounded-full h-3 w-4 
+                ${col.name === 'Todo'
+                  ? 'bg-[#49C4E5]'
+                  : col.name === 'Done'
+                    ? 'bg-[#67E2AE]'
+                    : 'bg-[#8471F2]'}`}>
+              </div>
+              <p className='uppercase pl-2 text-[#828FA3] font-bold text-xs tracking-[0.15rem] leading-[0.938rem]'>
+                {col.name} ({col.tasks.length})
+              </p>
+            </div>
           </div>
         ))}
     </section>

75c1ecc171d4509468952e8ac81c7b458f1e415d
HPHuerta <hhernanp@gmail.com>
2023-08-01 23:55:43 -0400

    feat: refactor form to make it reusable for other components

--- src/app/components/AddNewTask/AddNewTask.tsx
+++ src/app/components/AddNewTask/AddNewTask.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/app/core/utils/Button'
 import { type Column } from '@/app/lib/hooks/useGetBoards'
-import SubtaskSection from './SubtaskSection'
 import addTasks from '@/app/core/services/addTasks'
 import addSubTasks from '@/app/core/services/addSubTasks'
 import { useState } from 'react'
 import { newTask } from '@/app/lib/store/taskAdded'
+import Form from '../form/Form'
 
 interface AddNewTaskProps {
   setAddTaskModal: (value: boolean) => void
@@ -80,57 +79,7 @@ export default function AddNewTask ({ setAddTaskModal, column }: AddNewTaskProps
       <div className='bg-[#FFF] dark:bg-[#2B2C37] rounded-md flex flex-col gap-4 shadow-lg shadow-[#364e7e40]/25 absolute w-[18rem] top-[4.7rem] p-4'>
         <h2 className='capitalize text-[1.125rem] font-bold leading-normal'>add new task</h2>
 
-        {/* Form Section */}
-        <form
-          onSubmit={(e) => { handleSubmit(e) }}
-          className="flex flex-col gap-3 [&_label]:text-[0.75rem] [&_label]:font-bold [&_label]:leading-normal [&_label]:text-[#828FA3] [&_label]:dark:text-[#FFF]">
-
-          {/* Title form section */}
-          <label className="capitalize">title</label>
-          <input
-            type="text"
-            className={`rounded-[0.25rem] border ${titleFormValidation ? 'border-[red]' : 'border-[#828fa340]'}  bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] dark:placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent `}
-            placeholder="e.g. Take coffee break"
-            id='title'
-            name='title'
-          />
-
-          {/* Description form section */}
-          <label className="capitalize">description</label>
-          <textarea
-            name="description"
-            id="description"
-            cols={30}
-            rows={5}
-            placeholder="e.g. It’s always good to take a break. This 15 minute break will  recharge the batteries a little."
-            className={`rounded-[0.25rem] border ${descriptionFormValidation ? 'border-[red]' : 'border-[#828fa340]'}  bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] dark:placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent ` }
-          />
-
-          {/* Subtask form section */}
-          <SubtaskSection subTaskValidation={subtaskFormValidation}/>
-
-          {/* Status Form Section */}
-          <label className="capitalize">status</label>
-          <select name="status" id="status" className="capitalize rounded-[0.25rem] border bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent">
-            {
-              column.map((column: Column) => {
-                return (
-                  <option value={`${column.name}`} key={column.id}>{column.name}</option>
-                )
-              })
-            }
-          </select>
-
-          {/* Create Task Button */}
-          <Button
-            icon='./icons/icon-add-task-mobile.svg'
-            buttonStyle={'bg-[#635FC7] w-[100%] h-[2.5rem] flex justify-center items-center rounded-xl text-[#fff] font-bold text-[0.8125rem] leading-[1.4375rem] capitalize '}
-            imageClassName='hidden'
-            type='submit'
-          >
-            create task
-          </Button>
-        </form>
+        <Form onSubmit={(e) => { handleSubmit(e) } } titleFormValidation={titleFormValidation} descriptionFormValidation={descriptionFormValidation} subtaskFormValidation={subtaskFormValidation} column={column} />
 
       </div>
     </div>
---
+++ src/app/components/form/Form.tsx
@@ -0,0 +1,72 @@
+import { Button } from '@/app/core/utils/Button'
+import { type Column } from '@/app/lib/hooks/useGetBoards'
+import SubtaskSection from './SubtaskSection'
+
+interface FormProps {
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  titleFormValidation: boolean
+  descriptionFormValidation: boolean
+  subtaskFormValidation: boolean
+  column: Column[]
+}
+
+export default function Form ({
+  onSubmit,
+  titleFormValidation,
+  descriptionFormValidation,
+  subtaskFormValidation,
+  column
+}: FormProps) {
+  return (
+    <form
+    onSubmit={onSubmit}
+    className="flex flex-col gap-3 [&_label]:text-[0.75rem] [&_label]:font-bold [&_label]:leading-normal [&_label]:text-[#828FA3] [&_label]:dark:text-[#FFF]">
+
+    {/* Title form section */}
+    <label className="capitalize">title</label>
+    <input
+      type="text"
+      className={`rounded-[0.25rem] border ${titleFormValidation ? 'border-[red]' : 'border-[#828fa340]'}  bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] dark:placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent `}
+      placeholder="e.g. Take coffee break"
+      id='title'
+      name='title'
+    />
+
+    {/* Description form section */}
+    <label className="capitalize">description</label>
+    <textarea
+      name="description"
+      id="description"
+      cols={30}
+      rows={5}
+      placeholder="e.g. It’s always good to take a break. This 15 minute break will  recharge the batteries a little."
+      className={`rounded-[0.25rem] border ${descriptionFormValidation ? 'border-[red]' : 'border-[#828fa340]'}  bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] dark:placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent ` }
+    />
+
+    {/* Subtask form section */}
+    <SubtaskSection subTaskValidation={subtaskFormValidation}/>
+
+    {/* Status Form Section */}
+    <label className="capitalize">status</label>
+    <select name="status" id="status" className="capitalize rounded-[0.25rem] border bg-[#FFF] dark:bg-[#2B2C37] p-2 text-[0.8125rem] placeholder-[#000112] dark:placeholder-[#fff] placeholder-opacity-[0.25] focus:outline-none focus:ring-1 focus:ring-[#828fa340] focus:border-transparent">
+      {
+        column.map((column: Column) => {
+          return (
+            <option value={`${column.name}`} key={column.id}>{column.name}</option>
+          )
+        })
+      }
+    </select>
+
+    {/* Create Task Button */}
+    <Button
+      icon='./icons/icon-add-task-mobile.svg'
+      buttonStyle={'bg-[#635FC7] w-[100%] h-[2.5rem] flex justify-center items-center rounded-xl text-[#fff] font-bold text-[0.8125rem] leading-[1.4375rem] capitalize '}
+      imageClassName='hidden'
+      type='submit'
+    >
+      create task
+    </Button>
+  </form>
+  )
+}

d0c8f3356d172843164510be364624a3ebf8c54c
HPHuerta <hhernanp@gmail.com>
2023-09-27 23:48:09 -0400

    refactor: edit column to accept update and and columns

--- src/app/components/EditBoard/EditBoard.tsx
+++ src/app/components/EditBoard/EditBoard.tsx
@@ -5,6 +5,7 @@ import updateBoard from '@/app/core/services/updateBoard'
 import { updateBoardStore } from '@/app/lib/store/updateBoardStore'
 import updateColumn from '@/app/core/services/updateColumn'
 import { type EditBoardProps } from '@/app/lib/types/board'
+import addNewBoardColumns from '@/app/core/services/addNewBoardColumns'
 
 export default function EditBoard ({ setEditBoardModal }: EditBoardProps) {
   const [titleFormValidation, setTitleFormValidation] = useState(false)
@@ -49,11 +50,22 @@ export default function EditBoard ({ setEditBoardModal }: EditBoardProps) {
         }
       })
 
-      columnsToBeUpdated.map(async (column) => {
-        if (column.name) {
+      if (columnsToBeUpdated.length === 1) {
+        const column = columnsToBeUpdated[0]
+        if (column.id) {
           await updateColumn({ id: column.id, name: column.name })
+        } else {
+          await addNewBoardColumns({ name: column.name.toString(), boardId: id })
         }
-      })
+      } else if (columnsToBeUpdated.length > 1) {
+        columnsToBeUpdated.map(async (column) => {
+          if (column.name && column.id) {
+            await updateColumn({ id: column.id, name: column.name })
+          } else {
+            await addNewBoardColumns({ name: column.name.toString(), boardId: id })
+          }
+        })
+      }
 
       setBoardUpdated(true)
       form.reset()
